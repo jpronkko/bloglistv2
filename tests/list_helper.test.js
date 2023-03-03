782e4c9cb2d51f3c71b6b6bff717f70d1026e111
jpronkko <jukka.ronkko@iki.fi>
2023-03-03 10:21:30 +0200

    dummy list test removed

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -2,13 +2,6 @@ const listHelper = require('../utils/list_helper')
 const iniHelper = require('./test_helper')
 
 
-test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
-
 describe('total likes', () => {
 
   const listWithOneBlog = [iniHelper.singleBlog]
